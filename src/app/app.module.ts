40f651bbe31b3680fb8958cc020a5c3736fc7d0e
IanOSilva763 <ian.o.s@hotmail.com>
2023-09-24 17:17:56 -0300

    tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MattableComponent } from './mattable/mattable.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MattableComponent
+    ]
 })
 export class AppModule { }
---
+++ src/app/mattable/mattable.component.ts
@@ -0,0 +1,100 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+  import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+  import {MatSort, MatSortModule} from '@angular/material/sort';
+  import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+  import {MatInputModule} from '@angular/material/input';
+  import {MatFormFieldModule} from '@angular/material/form-field';
+
+  
+  export interface UserData {
+    id: string;
+    item: string;
+    quantidade: string;
+    cor: string;
+  }
+  
+  /** Constants used to fill up our data base. */
+  const CORES: string[] = [
+    'preto',
+    'roxo',
+    'vermelho',
+    'azul',
+    'rosa',
+    'verde',
+    'amarelo',
+  ];
+  const ITENS: string[] = [
+    'Lápis de cor',
+    'Caneta',
+    'Mochila',
+    'Post-it',
+    'Clips',
+    'Garrafa',
+    'Estojo',
+    'Caderno',
+    'Apontador',
+    'Régua',
+    'Tesoura',
+    'Calculadora',
+    'Corretivo',
+    'Cola',
+    'Agenda',
+    'Pasta',
+    'Adesivo',
+    'Carimbo',
+  ];
+  
+  /**
+   * @title Data table with sorting, pagination, and filtering.
+   */
+  @Component({
+    selector: 'app-mattable',
+    templateUrl: './mattable.component.html',
+    styleUrls: ['./mattable.component.css'],
+    standalone: true,
+    imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
+  })
+  export class MattableComponent implements AfterViewInit {
+    displayedColumns: string[] = ['id', 'item', 'quantidade', 'cor'];
+    dataSource: MatTableDataSource<UserData>;
+  
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
+  
+    constructor() {
+      // Create 100 users
+      const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+  
+      // Assign the data to the data source for the table to render
+      this.dataSource = new MatTableDataSource(users);
+    }
+  
+    ngAfterViewInit() {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }
+  
+    applyFilter(event: Event) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+  
+      if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+      }
+    }
+  }
+  
+  /** Builds and returns a new User. */
+  function createNewUser(id: number): UserData {
+    const item =
+      ITENS[Math.round(Math.random() * (ITENS.length - 1))] +
+      ' ' 
+  
+    return {
+      id: id.toString(),
+      item: item,
+      quantidade: Math.round(Math.random() * 15).toString(),
+      cor: CORES[Math.round(Math.random() * (CORES.length - 1))],
+    };
+  }
+  
