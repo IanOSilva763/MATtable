b272b451e6ed0f37ab6ac54778094443c015a37b
IanOSilva763 <ian.o.s@hotmail.com>
2023-09-25 21:30:21 -0300

    finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  urlImagem = "https://sonaesierracms-v2.cdnpservers.net/wp-content/uploads/sites/14/2018/08/01_variossc_produtosescolaresparaseufilho.jpg"
+
   title = 'mattableV';
+
+  ngOnInit(): void {}
 }
